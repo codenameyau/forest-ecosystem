1c4295bd59f6df60c0239e46e91428a98442eacd
Jorge Yau <jyau01@citymail.cuny.edu>
2014-08-03 20:48:33 -0400

    Adding keydown event and pause and resume to grid

--- cellular-automata/forest-ecosystem/main.js
+++ cellular-automata/forest-ecosystem/main.js
@@ -15,13 +15,14 @@
 function GridSimulation(settings) {
   this.initializeSettings(settings);
   this.initializeGrid(this.settings.gridSize);
-  console.log(this);
+  this.initializeEventHandlers();
 }
 
 GridSimulation.prototype.initializeSettings = function(settings) {
   this.settings = settings;
   this.checkProperty(this.settings, 'gridSize', 50);
   this.checkProperty(this.settings, 'cellSize', 5);
+  this.checkProperty(this.settings, 'running', true);
 };
 
 GridSimulation.prototype.initializeGrid = function(gridSize) {
@@ -31,22 +32,48 @@ GridSimulation.prototype.initializeGrid = function(gridSize) {
   }
 };
 
+GridSimulation.prototype.initializeEventHandlers = function() {
+  window.addEventListener('focus', this.resume.bind(this), false);
+  window.addEventListener('blur', this.pause.bind(this), false);
+  window.addEventListener('keydown', this._keyboardInputHandler.bind(this), false);
+};
+
 GridSimulation.prototype.checkProperty = function(object, property, value) {
   if (object && typeof object[property] === 'undefined') {
     object[property] = value;
   }
 };
 
+GridSimulation.prototype.pause = function() {
+  this.settings.running = false;
+  console.info('Paused');
+};
+
+GridSimulation.prototype.resume = function() {
+  this.settings.running = true;
+  console.info('Resuming');
+};
+
+GridSimulation.prototype.togglePause = function() {
+  if (this.settings.running) { this.pause(); }
+  else { this.resume(); }
+};
+
+GridSimulation.prototype._keyboardInputHandler = function(event) {
+  switch (event.which) {
+
+  case 32: // spacebar
+    this.togglePause();
+    break;
+
+  }
+};
 
 /******************************
  * GridCanvas - Visualization *
  ******************************/
 function GridCanvas(gridObject) {
   this.gridRef = gridObject;
-  // this.settings = {
-  //   cellSize: config.cellSize,
-  //   canvasID: config.canvasID,
-  // };
 }
 
 
