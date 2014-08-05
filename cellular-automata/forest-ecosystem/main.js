658d7ae51772f8a0000c453595fcb984322147d6
Jorge Yau <jyau01@citymail.cuny.edu>
2014-08-05 19:38:40 -0400

    Moving grid canvas to shared folder

--- cellular-automata/forest-ecosystem/main.js
+++ cellular-automata/forest-ecosystem/main.js
@@ -6,146 +6,70 @@
  * http://redd.it/27h53e
  * http://codegolf.stackexchange.com/q/35322/30051
  */
+
+/*---------------JSHint---------------*/
+/* global GridCanvas, GridSimulation  */
+/*------------------------------------*/
 'use strict';
 
 
-/******************************
- * GridCanvas - Visualization *
- ******************************/
-function GridCanvas(config) {
-  this.initializeSettings(config);
-  this.initializeCanvas();
-}
+/***************************
+ * Forest Life Constructor *
+ ***************************/
+function ForestLife(type) {
+  this.initializeProperties();
 
-GridCanvas.prototype.initializeSettings = function(settings) {
-  this.settings = settings;
-  this.checkProperty(this.settings, 'gridRows', 50);
-  this.checkProperty(this.settings, 'gridCols', 50);
-  this.checkProperty(this.settings, 'cellSize', 5);
-  this.checkProperty(this.settings, 'delay', 500);
-  this.checkProperty(this.settings, 'running', true);
-};
+  switch (this.lifeType) {
 
-GridCanvas.prototype.drawGrid = function(grid) {
-  this.ctx.strokeStyle = 'rgb(30, 30, 30)';
-  var radius = 4;
-  var cellSize = 10;
-  for (var i=0; i<grid.length; ++i) {
-    var rows = grid[i].length;
-    for (var j=0; j<rows; ++j) {
-      var posX = i*cellSize+this.settings.radius+1;
-      var posY = j*cellSize+radius+1;
-      this.ctx.moveTo(posX, posY);
-      this.ctx.beginPath();
-      this.ctx.arc(posX, posY, radius, 0, 2*Math.PI, true);
-      this.ctx.stroke();
-    }
-  }
-};
-
-GridCanvas.prototype.initializeCanvas = function() {
-  var canvas = document.getElementById(this.settings.canvasID);
-  canvas.width  = this.settings.gridCols * this.settings.cellSize;
-  canvas.height = this.settings.gridRows * this.settings.cellSize;
-  this.ctx = canvas.getContext('2d');
-};
-
-GridCanvas.prototype.checkProperty = function(object, property, value) {
-  if (object && typeof object[property] === 'undefined') {
-    object[property] = value;
-  }
-};
-
-
-/****************************
- * GridSimulation - Backend *
- ****************************/
-function GridSimulation(canvas) {
-  this.canvas = canvas;
-  this.initializeGrid();
-  this.initializeSimulation();
-  this.initializeEventHandlers();
-}
-
-GridSimulation.prototype.update = function() {
-  console.log('Updating');
-};
-
-GridSimulation.prototype.pause = function() {
-  this.simulation.running = false;
-  console.info('Paused');
-};
-
-GridSimulation.prototype.resume = function() {
-  this.simulation.running = true;
-  console.info('Resuming');
-};
-
-GridSimulation.prototype.togglePause = function() {
-  if (this.simulation.running) { this.pause(); }
-  else { this.resume(); }
-};
-
-GridSimulation.prototype.initializeGrid = function() {
-  var gridRows = this.canvas.settings.gridRows;
-  var gridCols = this.canvas.settings.gridCols;
-  this.grid = [];
-  for (var i=0; i<gridCols; i++) {
-    this.grid[i] = [];
-    for (var j=0; j<gridRows; j++) {
-      this.grid[i][j] = [];
-    }
-  }
-};
-
-GridSimulation.prototype.initializeSimulation = function() {
-  this.simulation = {
-    running: true,
-    time: 0,
-  };
-};
-
-GridSimulation.prototype.initializeEventHandlers = function() {
-  window.addEventListener('focus', this.resume.bind(this), false);
-  window.addEventListener('blur', this.pause.bind(this), false);
-  window.addEventListener('keydown', this._keyboardInputHandler.bind(this), false);
-};
-
-GridSimulation.prototype._keyboardInputHandler = function(event) {
-  switch (event.which) {
-
-  case 32: // spacebar
-    this.togglePause();
+  case 'sapling':
+    this.initializeSaplingTree();
+    break;
+  case 'tree':
+    this.initializeTree();
+    break;
+  case 'elder':
+    this.initializeElderTree();
+    break;
+  case 'lumberjack':
+    this.initializeLumberJack();
+    break;
+  case 'bear':
+    this.initializeBear();
     break;
 
   }
+}
+
+ForestLife.prototype.initializeProperties = function() {
+  this.age = 0;
+  this.movement = 0;
+  this.spawnProbability = 0.0;
+  this.spawnChild = '';
 };
 
-GridSimulation.prototype.getGrid = function() {
-  return this.grid;
+ForestLife.prototype.initializeSaplingTree = function() {
+  this.lifeID = 0;
+  this.lifeType = 'sapling';
 };
 
-GridSimulation.prototype.run = function() {
-  this.canvas.drawGrid(this.grid);
-  console.info('Running');
+ForestLife.prototype.initializeTree = function() {
+  this.lifeID = 1;
+  this.lifeType = 'tree';
+  this.spawnProbability = 0.1;
+  this.spawnChild = 'sapling';
 };
 
+ForestLife.prototype.initializeElderTree = function() {
+  this.lifeID = 2;
+  this.lifeType = 'elder';
+  this.spawnProbability = 0.2;
+  this.spawnChild = 'sapling';
+};
 
 
 /**************************
  * Program Initialization *
  **************************/
-function Life(options) {
-
-  this.movement = 0;
-}
-
-Life.prototype.checkProperty = function(object, property, value) {
-  if (object && typeof object[property] === 'undefined') {
-    object[property] = value;
-  }
-};
-
 (function() {
 
   // Specify configuration
@@ -159,10 +83,7 @@ Life.prototype.checkProperty = function(object, property, value) {
   };
 
   // Lumberjack Object
-  var JACK = {
-
-  };
-
+  var jack = new ForestLife('lumberjack');
 
   // GridCanvas: visualizes the simulation with canvas
   // GridSimulation: handles the backend simulation
@@ -172,7 +93,6 @@ Life.prototype.checkProperty = function(object, property, value) {
 
   // Generate forest ecosystem
   // Start: 10% lumberjacks, 50% trees, 2% bears
-
 
   console.log(simulation);
   // simulation.run();
