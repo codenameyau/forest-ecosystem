0a2c4d4ced76a3af9390b23bf23d2b7c87ab2abf
Jorge Yau <jyau01@citymail.cuny.edu>
2014-08-06 22:42:29 -0400

    Cleaning and refactoring forest life

--- public/cellular-automata/forest-ecosystem/main.js
+++ public/cellular-automata/forest-ecosystem/main.js
@@ -17,59 +17,57 @@
  * Forest Life Constructor *
  ***************************/
 function ForestLife(lifetype) {
-  this.initializeProperties(lifetype);
-
-  switch (lifetype) {
-
-  case 'sapling':
-    this.initializeSaplingTree();
-    break;
-  case 'tree':
-    this.initializeTree();
-    break;
-  case 'elder':
-    this.initializeElderTree();
-    break;
-  case 'lumberjack':
-    this.initializeLumberJack();
-    break;
-  case 'bear':
-    this.initializeBear();
-    break;
-
-  }
-}
-
-ForestLife.prototype.initializeProperties = function(lifetype) {
+  var parameters = this.parameters[lifetype];
   this.type = lifetype;
   this.age = 0;
   this.maturityAge = 0;
   this.movement = 0;
+  this.radius = parameters.radius.start;
+  this.growth = 0;
   this.spawnProbability = 0.0;
   this.spawnChild = '';
+}
+
+ForestLife.prototype.parameters = {
+  'sapling': {
+    maturity: {age: 12, previous: '', next: 'tree'},
+    radius: {start: 2, end: 5},
+    spawn: {chance: 0.0, child: ''},
+    movement: 0,
+  },
+
+  'tree': {
+    maturity: {age: 120, previous: 'sapling', next: 'elder'},
+    radius: {start: 5, end: 6},
+    spawn: {chance: 0.1, child: 'sapling'},
+    movement: 0,
+  },
+
+  'elder': {
+    maturity: {age: 0, previous: 'tree', next: ''},
+    radius: {start: 6, end: 6},
+    spawn: {chance: 0.2, child: 'sapling'},
+    movement: 0,
+  },
+
+  'lumberjack': {
+    maturity: {age: 0, previous: '', next: ''},
+    radius: {start: 4, end: 4},
+    spawn: {chance: 0.0, child: ''},
+    movement: 3,
+  },
+
+  'bear': {
+    maturity: {age: 0, previous: '', next: ''},
+    radius: {start: 5, end: 5},
+    spawn: {chance: 0.0, child: ''},
+    movement: 5,
+  },
 };
 
-ForestLife.prototype.initializeSaplingTree = function() {
-  this.maturityAge = 12;
-};
-
-ForestLife.prototype.initializeTree = function() {
-  this.spawnProbability = 0.1;
-  this.spawnChild = 'sapling';
-  this.maturityAge = 120;
-};
-
-ForestLife.prototype.initializeElderTree = function() {
-  this.spawnProbability = 0.2;
-  this.spawnChild = 'sapling';
-};
-
-ForestLife.prototype.initializeLumberJack = function() {
-  this.movement = 3;
-};
-
-ForestLife.prototype.initializeBear = function() {
-  this.movement = 5;
+ForestLife.prototype.grow = function() {
+  this.age++;
+  //Check maturity age
 };
 
 
@@ -111,16 +109,20 @@ ForestLife.prototype.initializeBear = function() {
     bear: 0,
   };
 
+  var tree = new ForestLife('tree');
+  console.log(tree);
+
   // Generate random forest ecosystem based on ratio
-  var gridSize = simulation.getSize();
-  for (var i=0; i<gridSize; i++) {
-    var randPos = simulation.randomPosition();
-    for (var v=0; v<2; v++) {
-      if (randPos[v] >= gridSize) {
-        console.log(false);
-      }
-    }
-  }
+  // var gridSize = simulation.getSize();
+  // for (var i=0; i<gridSize; i++) {
+  //   while (true) {
+  //     var pos = simulation.randomPosition();
+  //     if (!grid[pos[0]][pos[1]].length) {
+  //       grid[pos[0]][pos[1]].push();
+  //       break;
+  //     }
+  //   }
+  // }
 
   console.log(simulation);
   // simulation.run();
