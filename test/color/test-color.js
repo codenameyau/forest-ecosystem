b8f3be1a9100a2bfea4eef070070874277bf10d9
Jorge Yau <jyau01@citymail.cuny.edu>
2014-08-22 18:31:44 -0400

    Adding additional tests for color

--- test/color/test-color.js
+++ test/color/test-color.js
@@ -13,9 +13,6 @@
     var color = new Color();
 
     // Color default should be white
-    test.assertEqual(color.hex, 0xFFFFFF,
-      'hex value of color should be 0xFFFFFF');
-
     test.assertEqual(color.rgb.red, 255,
       'red value of color should be 255');
 
@@ -26,12 +23,82 @@
       'blue value of color should be 255');
   });
 
+  // Test Case: hexToRGB
+  test.testCase(function() {
+    var hexToRGB = Color.prototype.hexToRGB;
+    var upper = hexToRGB('FF250A');
+    var lower = hexToRGB('ff250a');
+
+    test.assertArray(upper,
+      'data type of rgb result should be an array');
+
+    test.assertArray(lower,
+      'data type of rgb result should be an array');
+
+    test.assertEqual(upper[0], 255,
+      'value of red channel should be 255');
+
+    test.assertEqual(upper[1], 37,
+      'value of green channel should be 37');
+
+    test.assertEqual(upper[2], 10,
+      'value of blue channel should be 10');
+
+    test.assertEqual(upper[0], lower[0],
+      'values of red channel should be equal');
+
+    test.assertEqual(upper[1], lower[1],
+      'values of green channel should be equal');
+
+    test.assertEqual(upper[2], lower[2],
+      'values of blue channel should be equal');
+  });
+
   // Test Case: hex constructor
   test.testCase(function() {
     var color = new Color('hex', 0xFF0000);
 
-    test.assertEqual(color.hex, 0xFF0000,
-      'hex value of color should be 0xFF0000');
+    test.assertEqual(color.rgb.red, 255,
+      'red value of color should be 255');
+
+    test.assertEqual(color.rgb.green, 0,
+      'green value of color should be 0');
+
+    test.assertEqual(color.rgb.blue, 0,
+      'blue value of color should be 0');
+  });
+
+  // Test Case: csshex constructor
+  test.testCase(function() {
+    var color = new Color('csshex', '#FF0000');
+
+    test.assertEqual(color.rgb.red, 255,
+      'red value of color should be 255');
+
+    test.assertEqual(color.rgb.green, 0,
+      'green value of color should be 0');
+
+    test.assertEqual(color.rgb.blue, 0,
+      'blue value of color should be 0');
+  });
+
+  // Test Case: rgb constructor
+  test.testCase(function() {
+    var color = new Color('rgb', [255, 0, 0]);
+
+    test.assertEqual(color.rgb.red, 255,
+      'red value of color should be 255');
+
+    test.assertEqual(color.rgb.green, 0,
+      'green value of color should be 0');
+
+    test.assertEqual(color.rgb.blue, 0,
+      'blue value of color should be 0');
+  });
+
+  // Test Case: cssrgb constructor
+  test.testCase(function() {
+    var color = new Color('cssrgb', 'rgb(255, 0, 0)');
 
     test.assertEqual(color.rgb.red, 255,
       'red value of color should be 255');
