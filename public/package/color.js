675273d0150eff3d04c695aef4cbd2978c4ea777
Jorge Yau <jyau01@citymail.cuny.edu>
2014-08-22 17:47:08 -0400

    Adding constructor parsing

--- public/package/color.js
+++ public/package/color.js
@@ -13,6 +13,49 @@
  * Color Constructor *
  *********************/
 function Color(type, value) {
-  console.log(type);
-  console.log(value);
+  switch (type) {
+
+  case 'hex':
+    this.parseHex(value);
+    break;
+
+  case 'csshex':
+    this.parseCSSHex(value);
+    break;
+
+  case 'cssrgb':
+    this.parseCSSRGB(value);
+    break;
+
+  default:
+    this.setDefaultColor();
+  }
 }
+
+Color.prototype.setDefaultColor = function() {
+  this.hex = 0xFFFFFF;
+  this.rgb = {
+    red: 255,
+    green: 255,
+    blue: 255
+  };
+};
+
+/****************************
+ * Color Constructor Parser *
+ ****************************/
+Color.prototype.parseHex = function(value) {
+
+};
+
+Color.prototype.parseCSSHex = function(value) {
+
+};
+
+Color.prototype.parseRGB = function(value) {
+
+};
+
+Color.prototype.parseCSSRGB = function(value) {
+
+};
