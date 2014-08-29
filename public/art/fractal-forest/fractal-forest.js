30641156818d251298882679441a4b46df4944ca
Jorge Yau <jyau01@citymail.cuny.edu>
2014-08-29 10:16:18 -0400

    Adding canvas to forest

--- public/art/fractal-forest/fractal-forest.js
+++ public/art/fractal-forest/fractal-forest.js
@@ -5,3 +5,21 @@
  * Challenge Description:
  * http://codegolf.stackexchange.com/q/35835
  */
+'use strict';
+
+var drawFractalTree = function(ctx) {
+
+};
+
+(function() {
+  var canvas = document.getElementById('imagination');
+  var ctx = canvas.getContext('2d');
+  var width = window.innerWidth;
+  var height = window.innerHeight;
+  canvas.width = width;
+  canvas.height = height;
+  canvas.style.width = width;
+  canvas.style.height = height;
+
+  ctx.fillRect(0, 0, 10, 10);
+})();
