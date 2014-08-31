d8753b871bd35e93f40518910ade9c9d2903aa4a
Jorge Yau <jyau01@citymail.cuny.edu>
2014-08-30 20:25:03 -0400

    Updating canvas drawing code

--- boilerplate/canvas/main.js
+++ boilerplate/canvas/main.js
@@ -3,6 +3,18 @@
 /********************
  * Canvas Utilities *
  ********************/
+var fillPixel = function(data, x, y, color) {
+  var index = (x + y * data.width) * 4;
+  data.data[index + 0] = color[0];
+  data.data[index + 1] = color[1];
+  data.data[index + 2] = color[2];
+  data.data[index + 3] = color[3] || 255;
+};
+
+var saveCanvasData = function(ctx, data) {
+  ctx.putImageData(data, 0, 0);
+};
+
 var clearCanvas = function(ctx) {
   ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 }
@@ -23,6 +35,9 @@ var clearCanvas = function(ctx) {
     cellSize: 5,
     numCells: 100,
   };
+
+  // Canvas settings
+  ctx.imageSmoothingEnabled = false;
 
   // Draw something
   ctx.fillStyle = 'rgb(200, 0, 0)';
